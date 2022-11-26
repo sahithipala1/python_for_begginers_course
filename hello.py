a85c8ec5758696e11a015995ee29ef5de85ce993
kranthigy <kranthi@goodyear.com>
2022-11-26 16:19:35 -0500

    Basic

--- hello.py
+++ hello.py
@@ -1,1 +1,32 @@
 print("HELLO WORLD")
+
+# Print the value of the Variable.
+
+age = 20
+print(age)
+
+# How to take input from user
+
+name = input("Please enter your name: ")
+
+print("Hello " + name)
+
+#   Current age of the user:
+
+birth_year = input("Please enter your birth year: ")
+age = 2022 - int(birth_year)
+print(f"your age is {age}")
+print(" your age is: " + str(age))
+
+# example one: print sum of the 2 numbers of the user given
+
+x = input("Please enter the first number: ")
+y = input("Please enter the second number: ")
+
+sum_of_two = float(x) + float(y)
+
+print(sum_of_two)
+
+# Srings
+
+
